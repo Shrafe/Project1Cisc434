e0fb63f2c9df455345ab8ee0913607fbc6c242a4
Thomas Hauser <tom.hauser@gmail.com>
2011-10-15 02:11:06 -0700

    cleaning up

--- src/project/socket/client/SocketClient.java
+++ src/project/socket/client/SocketClient.java
@@ -8,17 +8,14 @@ public class SocketClient {
 		Socket s = null; // the socket for communication and connecting to the server
 		boolean scenario2 = false;
 		int socket = 4444;
-			// 	we want to send 10 Payloads in parallel
-			// spawn a new ClientThread to send the payload to the socket and wait for the server's response
-			// generate 10 payloads and send them
-			//send only one payload
-			if (scenario2){
-				for (int i = 0; i < 10; i++){
-					new SocketClientThread(new Socket("localhost",4444)).start(); // 10 calls at the same time.
-				}
+		
+		if (scenario2){
+			for (int i = 0; i < 10; i++){
+				new SocketClientThread(new Socket("localhost",socket)).start(); // 10 calls at the same time.
 			}
-			else
-				new SocketClientThread(new Socket("localhost",4444)).start(); // 1 thread, calling ten times
+		}
+		else
+			new SocketClientThread(new Socket("localhost",socket)).start(); // 1 thread, calling ten times
 
 	}
 

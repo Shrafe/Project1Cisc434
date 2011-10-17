d6a83dabafdaf3624de297aceaa0252eff05d405
Thomas Hauser <tom.hauser@gmail.com>
2011-10-17 03:50:34 -0700

    similar changes to RPC; use Callable instead of Thread; wait for all threads to complete before reporting completed in Scenario2

--- src/project/rmi/client/RmiClientSender.java
+++ src/project/rmi/client/RmiClientSender.java
@@ -1,9 +1,11 @@
 package rmi.client;
 
 import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
 import rmi.server.RmiServer;
 
-public class RmiClientSender extends Thread{
+public class RmiClientSender implements Callable<double[]>{
 	public RmiServer server;
 	public ArrayList<double[]>payload;
 	public int clientNum;
@@ -16,17 +18,19 @@ public class RmiClientSender extends Thread{
 		this.threadNum = tn;
 	}
 	
-	public void run(){
+	public double[] call(){
 		long startTime = 0;
 		long timeTaken = 0;
 		long totalTime = 0;
+		double[] result = null;
 		try{
 			System.out.println("Client:"+clientNum+"|Thread:"+threadNum+": Sending payload ...");
 			startTime = System.currentTimeMillis(); // measuring only the time taken to transfer
-			server.getAverage(payload);
+			result = server.getAverage(payload);
 			timeTaken = System.currentTimeMillis() - startTime;
 			totalTime+=timeTaken;
 			System.out.println("Client:"+clientNum+"|Thread:"+threadNum+": Received result in: "+timeTaken+"ms");
 		}catch (Exception e){e.printStackTrace();}		
+		return result;
 	}
 }
